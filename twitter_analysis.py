cb467631f2096f0f9afba74fc7856712db5e1e7d
mathsten <stensrudmathias@gmail.com>
2017-11-02 22:03:12 +0100

    Update twitter_analysis.py

--- twitter_analysis.py
+++ twitter_analysis.py
@@ -5,11 +5,11 @@ import tweepy
 from textblob import TextBlob
 import csv
 
-consumer_key = 'OqriG8mBO5qw4mQd8mLYCR6QG'
-consumer_secret = 'DLjdp656W2CF1F6YgGNt0HqHumI42pjZDOYan0zTGhIH0y51jb'
+consumer_key = 'CONSUMER_KEY'
+consumer_secret = 'CONSUMER_SECRET'
 
-access_token = '21324391-F5umrr9f8DSASftbuLSNA6o8sDgqMTFY1KPrHaZ1r'
-access_token_secret = 'prln9lorsd0PW6bheopasUtPxAntmVCj5X8XrS53SvjUg'
+access_token = 'ACCESS_TOKEN'
+access_token_secret = 'ACCCESS_TOKEN_SECRET'
 
 # Setter opp forbindelse med twitter
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -38,14 +38,14 @@ with open('twitter_analyse.csv', 'w', newline='') as csvfile:
 # public_tweets = api.search(search_word, count = 100)
 
 # Only iterate through the first 20 statuses
-for status in tweepy.Cursor(api.user_timeline, id='OlavSakseid').items(20):
+for status in tweepy.Cursor(api.user_timeline, id='username').items(20):
     print(status.text)
     analysis = TextBlob(status.text)
     print(analysis)
     # writeCSV.writerow([encoded_string])
 
 '''
-OlavSakseid
+Username
 for tweet in public_tweets:
     #print(tweet.text)
     analysis = TextBlob(tweet.text)
